9a8a4026f5339f0dae552d7cca246e8ab6d849ae
Ammar Qammaz <ammarkov@gmail.com>
2017-08-08 02:22:44 +0300

    working on score4

--- score4/score4.c
+++ score4/score4.c
@@ -25,6 +25,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include <unistd.h>
 #include "../../AmmServerlib/AmmServerlib.h"
 
+#include "engine.h"
 
 
 
@@ -44,44 +45,174 @@ struct AmmServer_Instance  * default_server=0;
 
 struct AmmServer_RH_Context indexPage={0};
 struct AmmServer_RH_Context viewGame={0};
+struct AmmServer_RH_Context prepareGame={0};
+struct AmmServer_RH_Context findGame={0};
 struct AmmServer_RH_Context makeAMove={0};
 
 
-int activePlayer = 1;
-int width  =10;
-int height =10;
-int board[10*10]={0};
+void * pleaseLogin(struct AmmServer_DynamicRequest  * rqst)
+{
+    rqst->content[0]=0;
+    strcat(rqst->content,"<html>\n<body>\n");
+    strcat(rqst->content,"Please login\n");
+    strcat(rqst->content,"</body>\n</html>\n");
+    rqst->contentSize=strlen(rqst->content);
+}
+
+void * wrongGame(struct AmmServer_DynamicRequest  * rqst)
+{
+    rqst->content[0]=0;
+    strcat(rqst->content,"<html>\n<body>\n");
+    strcat(rqst->content,"Wrong Game Request\n");
+    strcat(rqst->content,"</body>\n</html>\n");
+    rqst->contentSize=strlen(rqst->content);
+}
+
+void * waitForGameToStart(struct AmmServer_DynamicRequest  * rqst)
+{
+
+  char playerID[128]={0};
+  char tableID[128]={0};
+
+  if (
+        ( _GET(default_server,rqst,"p",playerID,128) ) &&
+        ( _GET(default_server,rqst,"g",tableID,128) )
+     )
+  {
+    rqst->content[0]=0;
+    char buf[256];
+    snprintf(buf,256,"view.html?p=%s&g=%s&t=%u",playerID,tableID,rand()%10000);
+    strcat(rqst->content,"<html>\n");
+    strcat(rqst->content,"<head>\n");
+    strcat(rqst->content,"<meta http-equiv=\"refresh\" content=\"5; url=");
+    strcat(rqst->content,buf);
+    strcat(rqst->content,"\"></head><body>\n");
+    strcat(rqst->content,"Waiting for Game to Start\n");
+    strcat(rqst->content,"</body>\n</html>\n");
+    rqst->contentSize=strlen(rqst->content);
+  } else
+  {
+    pleaseLogin(rqst);
+  }
+}
+
+void * find_game_callback(struct AmmServer_DynamicRequest  * rqst)
+{
+   rqst->content[0]=0;
+  char playerID[128]={0};
+ if ( _GET(default_server,rqst,"p",playerID,128) )
+   {
+   strcat(rqst->content,"<html>\n<body>\n");
+   int i=0;
+   int emptyGamesShown=0;
+   for (i=0; i<50; i++)
+   {
+       char buf[256];
+       buf[0]=0;
+
+       if (games[i].started)
+       {
+         snprintf(buf,256,"<a href=\"view.html?p=%s&g=%u\">See Game %u ( %s vs %s )</a><br>",playerID,i,i,games[i].playerAName,games[i].playerBName );
+       } else
+       {
+         if (games[i].playersJoined>0)
+         {
+          snprintf(buf,256,"<a href=\"view.html?p=%s&g=%u\">Join New Game %u ( %s vs %s )</a><br>",playerID,i,i,games[i].playerAName,games[i].playerBName);
+         } else
+         if ( (emptyGamesShown==0) && (games[i].playersJoined==0) )
+         {
+          ++emptyGamesShown;
+          snprintf(buf,256,"<a href=\"view.html?p=%s&g=%u\">Join New Game %u</a><br>",playerID,i,i,games[i].playerAName,games[i].playerBName);
+         }
+       }
+
+       if (buf[0]!=0)
+         { strcat(rqst->content,buf); }
+       buf[0]=0;
+   }
+   strcat(rqst->content,"</body>\n</html>\n");
+   rqst->contentSize=strlen(rqst->content);
+   } else
+   {
+     pleaseLogin(rqst);
+   }
+}
+
+
+
+
+
+
+void * prepare_game_callback(struct AmmServer_DynamicRequest  * rqst)
+{
+  char playerAID[128]={0};
+  char playerBID[128]={0};
+  char tableID[128]={0};
+  if ( _GET(default_server,rqst,"pA",playerAID,128) )
+     {
+     }
+  if ( _GET(default_server,rqst,"pB",playerBID,128) )
+     {
+     }
+  if ( _GET(default_server,rqst,"g",tableID,128) )
+     {
+     }
+}
 
 //This function prepares the content of  stats context , ( stats.content )
 void * prepare_view_content_callback(struct AmmServer_DynamicRequest  * rqst)
 {
-  time_t t = time(NULL);
-  struct tm tm = *localtime(&t);
+  char playerID[128]={0};
+  char tableID[128]={0};
+  unsigned int gameID;
 
-  struct AmmServer_MemoryHandler * viewGameHTMLWriteable = AmmServer_CopyMemoryHandler(viewGameHTML);
-
-  char * boardRendering = (char*) malloc(sizeof(char) * 149000);
-  char buf[4096];
-  if (boardRendering!=0)
+  if (
+        ( _GET(default_server,rqst,"p",playerID,128) ) &&
+        ( _GET(default_server,rqst,"g",tableID,128) )
+     )
   {
+   gameID=atoi(tableID);
+
+   if (gameID>50)
+   {
+       wrongGame(rqst);
+   } else
+   {
+
+    if ( games[gameID].playersJoined<2 )
+    {
+       waitForGameToStart(rqst);
+    } else
+    {
+   unsigned int id=0;
+   time_t t = time(NULL);
+   struct tm tm = *localtime(&t);
+   char buf[4096];
+
+   struct AmmServer_MemoryHandler * viewGameHTMLWriteable = AmmServer_CopyMemoryHandler(viewGameHTML);
+
+   snprintf(buf,4096,"view.html?p=%s&g=%s&t=%u",playerID,tableID,rand()%10000);
+   AmmServer_ReplaceAllVarsInMemoryHandler(viewGameHTMLWriteable,1,"$REFRESH_LINK_FOR_PARTICULAR_PERSON$", buf);
+
+   char * boardRendering = (char*) malloc(sizeof(char) * 149000);
+   if (boardRendering!=0)
+   {
     char * boardRenderingPTR = boardRendering;   *boardRenderingPTR=0;
 
-    strcat(boardRendering,"<center>\n");
-    strcat(boardRendering," <div class=\"board\">\n");
     strcat(boardRendering,"  <table width=\"1000\" border=1>\n");
     unsigned int x,y,i=0;
-    for (y=0; y<height; y++)
+    for (y=0; y<games[id].height; y++)
     {
       strcat(boardRendering,"   <tr>\n");
-      for (x=0; x<width; x++)
+      for (x=0; x<games[id].width; x++)
       {
          strcat(boardRendering,"     <td width=90>\n");
-          if ( board[i]==1 ) { snprintf(buf,4096,"      <img src=\"textures/circle.png\" width=90>\n",i); } else
-          if ( board[i]==2 ) { snprintf(buf,4096,"      <img src=\"textures/x.png\" width=90>\n",i);      } else
-                             {
-                                snprintf(buf,4096,"      <a href=\"makeMove.html?s=%u&t=%02d-%02d-%02d_%02d:%02d:%02d\"><img src=\"textures/empty.png\" width=90></a>\n",i,
+          if ( games[id].board[i]==1 ) { snprintf(buf,4096,"      <img src=\"textures/circle.png\" width=90>\n",i); } else
+          if ( games[id].board[i]==2 ) { snprintf(buf,4096,"      <img src=\"textures/x.png\" width=90>\n",i);      } else
+                                       {
+                                         snprintf(buf,4096,"      <a href=\"makeMove.html?s=%u&t=%02d-%02d-%02d_%02d:%02d:%02d\"><img src=\"textures/empty.png\" width=90></a>\n",i,
                                          tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900,   tm.tm_hour, tm.tm_min, tm.tm_sec);
-                             }
+                                       }
 
          strcat(boardRendering,buf);
          strcat(boardRendering,"     </td>\n");
@@ -90,21 +221,24 @@ void * prepare_view_content_callback(struct AmmServer_DynamicRequest  * rqst)
       strcat(boardRendering,"   </td>\n");
     }
     strcat(boardRendering,"  </table>\n");
-    strcat(boardRendering," </div>\n");
-    strcat(boardRendering,"</center>\n");
 
 
     AmmServer_ReplaceAllVarsInMemoryHandler(viewGameHTMLWriteable,1,"$BOARD$", boardRendering);
 
     free(boardRendering);
+
+    snprintf(rqst->content,rqst->MAXcontentSize,"%s", viewGameHTMLWriteable->content );
+    rqst->contentSize=strlen(rqst->content);
+    AmmServer_FreeMemoryHandler(&viewGameHTMLWriteable);
+    }
+   }
+   }
+
+  } else
+  {
+     pleaseLogin(rqst);
   }
 
-  //No range check but since everything here is static max_stats_size should be big enough not to segfault with the strcat calls!
-  snprintf(rqst->content,rqst->MAXcontentSize,"%s", viewGameHTMLWriteable->content );
-  rqst->contentSize=strlen(rqst->content);
-
-
-  AmmServer_FreeMemoryHandler(&viewGameHTMLWriteable);
   return 0;
 }
 
@@ -112,20 +246,24 @@ void * prepare_view_content_callback(struct AmmServer_DynamicRequest  * rqst)
 
 void * prepare_move_content_callback(struct AmmServer_DynamicRequest  * rqst)
 {
+  unsigned int id=0;
   char moveRequested[128]={0};
   unsigned int move=1000000;
   if ( _GET(default_server,rqst,"s",moveRequested,128) )
      {
-      fprintf(stderr,"Requested move %s for player %u\n",moveRequested,activePlayer);
+      fprintf(stderr,"Requested move %s for player %u\n",moveRequested,games[id].activePlayer);
       move=atoi(moveRequested);
      }
 
-   if (move<width*height)
+   if (move<games[id].width*games[id].height)
    {
-    board[move]=activePlayer;
+    if (games[id].board[move]==0)
+    {
+     games[id].board[move]=games[id].activePlayer;
 
-    if (activePlayer==1) { activePlayer=2; } else
-    if (activePlayer==2) { activePlayer=1; }
+     if (games[id].activePlayer==1) { games[id].activePlayer=2; } else
+     if (games[id].activePlayer==2) { games[id].activePlayer=1; }
+    }
    }
    return prepare_view_content_callback(rqst);
 }
@@ -136,10 +274,12 @@ void * prepare_move_content_callback(struct AmmServer_DynamicRequest  * rqst)
 void init_dynamic_content()
 {
 
-  if (! AmmServer_AddResourceHandler(default_server,&viewGame,"/view.html",webserver_root,150000,0,&prepare_view_content_callback,DIFFERENT_PAGE_FOR_EACH_CLIENT) )  { AmmServer_Warning("Failed adding stats page\n"); }
-  if (! AmmServer_AddResourceHandler(default_server,&indexPage,"/index.html",webserver_root,150000,0,&prepare_view_content_callback,DIFFERENT_PAGE_FOR_EACH_CLIENT) )  { AmmServer_Warning("Failed adding stats page\n"); }
-
+  if (! AmmServer_AddResourceHandler(default_server,&viewGame,"/view.html",webserver_root,150000,0,&prepare_view_content_callback,DIFFERENT_PAGE_FOR_EACH_CLIENT) )       { AmmServer_Warning("Failed adding stats page\n"); }
   if (! AmmServer_AddResourceHandler(default_server,&makeAMove,"/makeMove.html",webserver_root,150000,0,&prepare_move_content_callback,DIFFERENT_PAGE_FOR_EACH_CLIENT) )  { AmmServer_Warning("Failed adding stats page\n"); }
+  if (! AmmServer_AddResourceHandler(default_server,&prepareGame,"/prepareGame.html",webserver_root,45000,0,&prepare_game_callback,DIFFERENT_PAGE_FOR_EACH_CLIENT) )      { AmmServer_Warning("Failed adding stats page\n"); }
+  if (! AmmServer_AddResourceHandler(default_server,&findGame,"/index.html",webserver_root,145000,0,&find_game_callback,DIFFERENT_PAGE_FOR_EACH_CLIENT) )                  { AmmServer_Warning("Failed adding stats page\n"); }
+
+
 
   AmmServer_DoNOTCacheResourceHandler(default_server,&viewGame);
   AmmServer_DoNOTCacheResourceHandler(default_server,&indexPage);
@@ -148,6 +288,7 @@ void init_dynamic_content()
 
   viewGameHTML=AmmServer_ReadFileToMemoryHandler("src/Games/score4/res/board.html");
 
+  initializeGame(games,0);
 
 }
 
